938e189a2f3266e7dcdd2612db6b216324c8a63e
Talha Sajid <86770288+IamTalha-Sajid@users.noreply.github.com>
2022-05-19 17:19:25 +0500

    Added Comments

--- ERC20Staking.sol
+++ ERC20Staking.sol
@@ -26,16 +26,19 @@ contract Staking {
 
     constructor() {}
 
+    //Sets the Address of the Token User Wanna Stake
     function setERC20Token(address _token) public returns(bool){
         token = IERC20(_token);
         return true;
     }
 
+    //Function to set the Reward ERC20 Token
     function setRewardToken(address _token) public returns(bool){
         rewardToken = IERC20(_token);
         return true;
     }
 
+    //Function to Stake Tokens
     function addStake(address _user, uint _amount) public returns (bool){
         require (_amount <= token.allowance(_user, address(this)), "You can't Stake More than You Own");
         stakes[_user] = stake(_user, _amount, block.timestamp, true);
@@ -43,17 +46,20 @@ contract Staking {
         return true;
     }
 
+    //Function to Claim Reward While Staking
     function reward(address _user) public returns(bool) {
         require(stakes[_user].staked == true, "You Haven't Staked any Tokens");
         rewardToken.mintReward(_user);
         return true;
     }
 
+    //Function to Unstake Tokens
     function checkStakedToken (address _user) public view returns (uint){
         require(stakes[_user].staked == true, "You Haven't Staked any Tokens");
         return stakes[_user].amount;
     }
 
+    //Function to Unstake Tokens
     function removeStake (address _user, uint _amount) public returns (bool){
         require(stakes[_user].staked == true, "You Haven't Staked any Tokens");
         require(stakes[_user].amount >= _amount, "You can't Unstake more than you have Staked");
@@ -66,10 +72,12 @@ contract Staking {
         return true;
     }
 
+    //Check the Token Balance Staked in the Contract
     function checkContractBalance () public view returns(uint256){
         return token.balanceOf(address(this));
     }
 
+    //Check If User is Currently Staking or Not
     function checkStakingStatus(address _user) public view returns(bool){
         return (stakes[_user].staked);
     }
